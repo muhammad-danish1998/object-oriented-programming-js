632bf5b90c4c3dea4fb7b2a04a60392e434774ca
muhammad-danish1998 <iamdanish1998@gmail.com>
2022-12-28 19:28:42 +0500

    add few lines

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
+// ----------- key pints -------------
+
+// if we put static before instance method then inheritance can not
+// access static method
+//we can acces instance method
+//for inheritance we use extend
+// we have also use super funtion in inherited before function
+//contructor funtion auto call when object is created and all properties will be 
+// define in contructor function
+// static function can not be call by object 
+// ------------------------------------------
+// --------------- creating class ------------
+
 class Student {
-    constructor(name , study){
-        this.name = name;
-        this.study = study
+    constructor(name,age){
+        this.name = name
+        this.age = age;
+        console.log("Contructor Function");
     }
-  bioData() {
-    console.log(`My nameis ${this.name} `);
-  }
-  mystudy(){
-    console.log(`i am stdying ${this.study}`);
-  }
+    getData(){
+        console.log(`My name is ${this.name} and my age is ${this.age}`);
+    }
+    static coffe(){
+        console.log("hello i need coffe");
+    }
 }
-let obj1 = new Student("Danish" , "CS");
-obj1.bioData();
-obj1.mystudy()
+//  a = new Student("Danish");
+ b = new Student("ALi" , 25);
+
+//  a.getData()
+ b.getData()
+ Student.coffe()
+
+
+
